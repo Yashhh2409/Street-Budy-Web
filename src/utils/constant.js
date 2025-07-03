065eda666f37a4364dd19550e3e892961273c9d6
Yash Dhande <dhande.yash2001@gmail.com>
2025-07-03 19:57:36 +0530

    Category, Restaurant Details page updated 2

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
 
 export const IMAGE_BASE_URL = "https://admin.streetbuddy.in";
-export const API_BASE_URL = "http://localhost:3000"
+export const API_BASE_URL = "https://street-budy-web.vercel.app"
 export const CURRENCY_SYMBOL = "₹";
