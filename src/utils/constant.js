97b48bde1d845778ac941da70a331689f9042012
Yash Dhande <dhande.yash2001@gmail.com>
2025-07-23 15:26:47 +0530

    Responsive Changes

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,5 @@
 
 export const IMAGE_BASE_URL = "https://admin.streetbuddy.in";
-export const API_BASE_URL = "http://localhost:3000"
+// export const API_BASE_URL = "http://localhost:3000"
+export const API_BASE_URL = "https://street-budy-web.onrender.com/"
 export const CURRENCY_SYMBOL = "₹";
